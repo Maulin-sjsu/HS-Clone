9e79f3d4ab891835dc9ba8b4168e6b80a5e8410b
Maulin Bodiwala <maulin.bodiwala@sjsu.edu>
2020-04-25 17:43:26 -0700

    Added few more routes and dynamic pages

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,21 @@ const router = express.Router()
 // module.exports = router;
 
 
-module.exports = ()=>{
+module.exports = (params)=>{
     router.get('/',(req,res)=>{
+
+        // if(!req.session.visitcount){
+        //     req.session.visitcount = 0;
+        // }
+        // req.session.visitcount += 1;
+        // console.log('Number of Visits : '+ req.session.visitcount);
+
     // res.sendFile(path.join(__dirname,'./static/index.html'))
-    res.render('pages/index',{pageTitle: 'Welcome'})
+    res.render('layouts',{pageTitle: 'Welcome',template: 'index'})
 });
 
-router.use('/speakers', speakersRoute())
-router.use('/feedback', feedbackRoute())
+router.use('/speakers', speakersRoute(params))
+router.use('/feedback', feedbackRoute(params))
 
 return router;
 };
